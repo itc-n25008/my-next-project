79b2b7899088bf12bcbeec9a23e8089233794687
n25008 <n25008@std.it-college.ac.jp>
2026-01-28 10:41:57 +0900

    7章まで完了

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,8 +1,19 @@
 import { getNewsList } from "@/app/libs/microcms";
 import NewsList from "@/app/components/NewsList";
+import Pagination from "../components/Pagination";
+import SearchField from "../components/SearchField";
+import { NEWS_LIST_LIMIT } from "@/app/constants";
 
 export default async function Page() {
-  const { contents: news } = await getNewsList();
+  const { contents: news, totalCount } = await getNewsList({
+    limit: NEWS_LIST_LIMIT,
+  });
 
-  return <NewsList news={news} />;
+  return (
+    <>
+      <SearchField />
+      <NewsList news={news} />
+      <Pagination totalCount={totalCount} />
+    </>
+  );
 }
