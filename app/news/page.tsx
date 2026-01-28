f025f9b58526100764d44868dd8246888d87f7f8
n25008 <n25008@std.it-college.ac.jp>
2026-01-28 12:10:08 +0900

    サイト全体にrevalidateを設定する

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -4,8 +4,6 @@ import Pagination from "../components/Pagination";
 import SearchField from "../components/SearchField";
 import { NEWS_LIST_LIMIT } from "@/app/constants";
 
-export const revalidate = 0;
-
 export default async function Page() {
   const { contents: news, totalCount } = await getNewsList({
     limit: NEWS_LIST_LIMIT,
